310caa12098d0c926bc18e873d7b4fc1f0ee5f46
Kyle Hailey <kylelf@gmail.com>
2013-02-01 08:24:01 -0800

    added support for MacosX

---
+++ oramon_setup.sql
@@ -0,0 +1,56 @@
+column seq_ms for 9999.99
+   column seq_ct for 9999.99
+   column lfpw_ms for 9999.99
+   column lfpw_ct for 9999.99
+   column seq_ms for 9999.99
+   column scat_ct for 9999.99
+   column dpr_ms for 9999.99
+   column dpr_ct for 9999.99
+   column dprt_ms for 9999.99
+   column dprt_ct for 9999.99
+   column prevdprt_ct new_value prevdprt_ct_var
+   column prevdprt_tm new_value prevdprt_tm_var
+   column prevdpwt_ct new_value prevdpwt_ct_var
+   column prevdpwt_tm new_value prevdpwt_tm_var
+   column prevdpr_ct new_value prevdpr_ct_var
+   column prevdpr_tm new_value prevdpr_tm_var
+   column prevdpw_ct new_value prevdpw_ct_var
+   column prevdpw_tm new_value prevdpw_tm_var
+   column prevseq_ct new_value prevseq_ct_var
+   column prevseq_tm new_value prevseq_tm_var
+   column prevscat_ct new_value prevscat_ct_var
+   column prevscat_tm new_value prevscat_tm_var
+   column prevlfpw_ct new_value prevlfpw_ct_var
+   column prevlfpw_tm new_value prevlfpw_tm_var
+   column prevsec new_value prevsec_var
+   select 0 prevsec from dual;
+   select 0 prevseq_tm from dual;
+   select 0 prevseq_ct from dual;
+   select 0 prevscat_ct from dual;
+   select 0 prevscat_tm from dual;
+   select 0 prevlfpw_ct from dual;
+   select 0 prevlfpw_tm from dual;
+   select 0 prevdprt_ct from dual;
+   select 0 prevdprt_tm from dual;
+   select 0 prevdpwt_ct from dual;
+   select 0 prevdpwt_tm from dual;
+   select 0 prevdpr_ct from dual;
+   select 0 prevdpr_tm from dual;
+   select 0 prevdpw_ct from dual;
+   select 0 prevdpw_tm from dual;
+   column prevdprt_ct noprint
+   column prevdprt_tm noprint
+   column prevdpwt_ct noprint
+   column prevdpwt_tm noprint
+   column prevdpr_ct noprint
+   column prevdpr_tm noprint
+   column prevdpw_ct noprint
+   column prevdpw_tm noprint
+   column prevseq_ct noprint
+   column prevseq_tm noprint
+   column prevscat_ct noprint
+   column prevscat_tm noprint
+   column prevlfpw_ct noprint
+   column prevlfpw_tm noprint
+   column prevsec noprint
+
